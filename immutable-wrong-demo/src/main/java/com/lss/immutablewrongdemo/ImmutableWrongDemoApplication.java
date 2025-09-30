94a7ec41ac2526f333662042067d55bcdbc788f0
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-09-30 15:08:56 +0300

    1 commit

---
+++ immutable-wrong-demo/src/main/java/com/lss/immutablewrongdemo/ImmutableWrongDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lss.immutablewrongdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmutableWrongDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmutableWrongDemoApplication.class, args);
+    }
+
+}
---
+++ immutable-wrong-demo/src/test/java/com/lss/immutablewrongdemo/ImmutableWrongDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lss.immutablewrongdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmutableWrongDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
